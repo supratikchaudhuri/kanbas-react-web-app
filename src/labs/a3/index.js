6ffd2440f3184303942feef7dcdbd4d4ca8eb411
supra <rikc.2000@gmail.com>
2023-10-06 16:21:46 -0400

    lab complete except 2.2.7

--- src/labs/a3/index.js
+++ src/labs/a3/index.js
@@ -2,12 +2,18 @@ import JavaScript from "./JavaScript";
 import PathParameters from "./PathParameters";
 import Classes from "./Classes";
 import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoItem from "./todo/TodoItem";
+import TodoList from "./todo/TodoList";
 
 function Assignment3() {
   return (
     <div className="container">
       <h1>Assignment 3</h1>
 
+      <TodoItem/>
+      <TodoList/>
+      <ConditionalOutput/>
       <Styles/>
       <Classes/>
       <PathParameters/>
