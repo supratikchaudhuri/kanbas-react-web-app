6949f9beb59cc1a9658db6e05a2495ee65e797ea
supra <rikc.2000@gmail.com>
2023-11-09 20:07:56 -0500

    lab fixed

---
+++ src/labs/a5/SimpleAPIExamples/WorkingWithObjects.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function WorkingWithObjects() {
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
+  const URL = `${BASE_URL}/a5/assignment`;
+
+  const [assignment, setAssignment] = useState({
+    id: 1,
+    title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10",
+    completed: false,
+    score: 0,
+  });
+  const fetchAssignment = async () => {
+    const response = await axios.get(`${URL}`);
+    setAssignment(response.data);
+  };
+
+  const updateTitle = async () => {
+    const response = await axios.get(`${URL}/title/${assignment.title}`);
+    setAssignment(response.data);
+  };
+  useEffect(() => {
+    fetchAssignment();
+  }, []);
+
+  return (
+    <div>
+      <h3>Working With Objects</h3>
+      <h3>Modifying Properties</h3>
+      <a
+        href={`${URL}/title/${assignment.title}`}
+        className="btn btn-primary me-2 float-end"
+      >
+        Update Title
+      </a>
+      <input
+        onChange={(e) =>
+          setAssignment({ ...assignment, title: e.target.value })
+        }
+        value={assignment.title}
+        className="form-control mb-2 w-75"
+        type="text"
+      />
+
+      <button onClick={updateTitle} className="w-100 btn btn-primary mb-2">
+        Update Title to: {assignment.title}
+      </button>
+      <button onClick={fetchAssignment} className="w-100 btn btn-danger mb-2">
+        Fetch Assignment
+      </button>
+
+      <a
+        href={`${URL}/score/${assignment.score}`}
+        className="btn btn-primary me-2 float-end"
+      >
+        Update Score
+      </a>
+      <input
+        onChange={(e) =>
+          setAssignment({ ...assignment, score: e.target.value })
+        }
+        value={assignment.score}
+        className="form-control mb-2 w-75"
+        type="number"
+      />
+
+      <a
+        href={`${URL}/completed/${assignment.completed}`}
+        className="btn btn-primary me-2 float-end"
+      >
+        Update Completed
+      </a>
+      <input
+        onChange={(e) =>
+          setAssignment({
+            ...assignment,
+            completed: e.target.checked,
+          })
+        }
+        checked={assignment.completed}
+        className="form-check-input mb-2"
+        type="checkbox"
+        id="isCompleted"
+      />
+      <label className="form-check-label" for="isCompleted">
+        Completed
+      </label>
+
+      <h3>Retrieving Objects</h3>
+      <a
+        href={`${BASE_URL}/a5/assignment`}
+        className="btn btn-primary me-2 w-25"
+      >
+        Get Assignment
+      </a>
+      <h3>Retrieving Properties</h3>
+      <a
+        href={`${BASE_URL}/a5/assignment/title`}
+        className="btn btn-primary me-2 w-25"
+      >
+        Get Title
+      </a>
+    </div>
+  );
+}
+
+export default WorkingWithObjects;
--- src/labs/a5/index.js
+++ src/labs/a5/index.js
@@ -1,33 +1,16 @@
-import EncodingParametersInURLs from "./EncodingParamsInURL";
-import WorkingWithArrays from "./WorkingWithArrays";
-import WorkingWithObjects from "./WorkingWithObjets";
+import SimpleAPIExamples from "./SimpleAPIExamples";
 
 function Assignment5() {
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
   return (
     <div className="container">
       <h1>Assignment 5</h1>
-      <div className="list-group mb-5">
-        <a href="http://localhost:4000/a5/welcome" className="list-group-item">
+      <div className="list-group">
+        <a href={`${BASE_URL}/a5/welcome`} className="list-group-item">
           Welcome
         </a>
       </div>
-
-      {/* 3.1 */}
-      <div className="mb-5">
-        <EncodingParametersInURLs />
-      </div>
-
-      {/* 3.2 */}
-      <div className="mb-5">
-        <WorkingWithObjects />
-      </div>
-
-      {/* 3.3  incomplete */}
-      <div className="mb-5">
-        <WorkingWithArrays />
-      </div>
-
-      {/* 3.4 */}
+      <SimpleAPIExamples />
     </div>
   );
 }
